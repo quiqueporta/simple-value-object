d6fd61f921cf6c81335fcc2fd5f0ee27b766e6ca
Quique Porta <me@quiqueporta.com>
2022-10-27 19:28:55 +0200

    Create a base exception to make capture errors easier

--- simple_value_object/exceptions.py
+++ simple_value_object/exceptions.py
@@ -1,19 +1,23 @@
-class ConstructorWithoutArguments(Exception):
+class SimpleValueObjectException(Exception):
+    pass
+
+
+class ConstructorWithoutArguments(SimpleValueObjectException):
     def __init__(self):
         super().__init__('No arguments declared in __init__')
 
 
-class CannotBeChanged(Exception):
+class CannotBeChanged(SimpleValueObjectException):
     def __init__(self):
         super().__init__(
             'You cannot change values from a Value Object, create a new one'
         )
 
 
-class InvariantViolation(Exception):
+class InvariantViolation(SimpleValueObjectException):
     pass
 
 
-class InvariantMustReturnBool(Exception):
+class InvariantMustReturnBool(SimpleValueObjectException):
     def __init__(self):
         super().__init__('Invariants must return a boolean value')
